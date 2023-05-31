97c645fad6bdf41d202367b71bd808d72d94646b
Stephanie Tran <stephanietran@Stephanies-MacBook-Air.local>
2023-05-30 18:46:20 -0700

    message

--- src/components/Section2.tsx
+++ src/components/Section2.tsx
@@ -1,15 +1,16 @@
 import '../styles/section2.scss';
+import penguin from '/Users/stephanietran/Desktop/react-team-practice-s23/src/components/IMG_1236.jpeg';
 export default function Section2(): JSX.Element {
   return (
     <div id="section2">
       <h1>Stephanie Tran!</h1>
-      <p>About Me</p>
+      <p className="test">About Me</p>
       <ul>
-        <li>I chewed on a fly once because I thought it was a boba ball...</li>
-        <li>I can make a star shape with a rubber band and my fingers!</li>
-        <li>I have been to two BTS concerts.</li>
+        <li id="firstBullet">I chewed on a fly once because I thought it was a boba ball...</li>
+        <li id="secondBullet">I can make a star shape with a rubber band and my fingers!</li>
+        <li id="thirdBullet">I have been to two BTS concerts.</li>
       </ul>
-      <img src="https://qph.cf2.quoracdn.net/main-qimg-e868e60f1df96d473dc3cf6ef713e640-lq" />
+      <img src={penguin} height="300px" />
     </div>
   );
 }
